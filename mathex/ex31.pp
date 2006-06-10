3d0df1207643cb66342709f0d8a5970671508832
daniel <daniel@freepascal.org>
2006-06-10 22:22:54 +0000

    * Simplify example
    git-svn-id: trunk@118 -

--- mathex/ex31.pp
+++ mathex/ex31.pp
@@ -1,33 +1,31 @@
-Program Example26;
+program Example31;
 
 { Program to demonstrate the MinValue function. }
 
-{ Make sore integer is 32 bit}
+{ Make sure integer is 32 bit}
 {$mode objfpc}
 
-Uses math;
+uses math;
 
-Type
-  TExFloatArray = Array[1..100] of Float;
-  TExIntArray = Array[1..100] of Integer;
-
-Var
-  I : Integer;
-  ExFloatArray : TExFloatArray;
-  AFloatArray : PFloat;
-  ExIntArray : TExIntArray;
-  AintArray : PInteger;
+var i:1..100;
+    f_array:array[1..100] of Float;
+    i_array:array[1..100] of Integer;
+    Pf_array:Pfloat;
+    PI_array:Pinteger;
 
 begin
-  Randomize;
-  AFloatArray:=@ExFloatArray[0];
-  AIntArray:=@ExIntArray[0];
-  for I:=1 to 100 do
-    ExFloatArray[i]:=(Random-Random)*100;
-  for I:=1 to 100 do
-    ExIntArray[i]:=Random(I)-Random(100);
-  Writeln('Min Float       : ',MinValue(ExFloatArray):8:4);
-  Writeln('Min Float   (b) : ',MinValue(AFloatArray,100):8:4);
-  Writeln('Min Integer     : ',MinValue(ExIntArray):8);
-  Writeln('Min Integer (b) : ',MinValue(AintArray,100):8);
+  randomize;
+
+  Pf_array:=@f_array[1];
+  Pi_array:=@i_array[1];
+
+  for i:=low(f_array) to high(f_array) do
+    f_array[i]:=(random-random)*100;
+  for i:=low(i_array) to high(i_array) do
+    i_array[i]:=random(I)-random(100);
+
+  Writeln('Min Float       : ',MinValue(f_array):8:4);
+  Writeln('Min Float   (b) : ',MinValue(Pf_array,100):8:4);
+  Writeln('Min Integer     : ',MinValue(i_array):8);
+  Writeln('Min Integer (b) : ',MinValue(Pi_array,100):8);
 end.
