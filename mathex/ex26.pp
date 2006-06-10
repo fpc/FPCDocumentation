fc09cdfdd246cdd57146af58affffde6c0389afe
daniel <daniel@freepascal.org>
2006-06-10 22:18:46 +0000

    * Simplify example
    git-svn-id: trunk@113 -

--- mathex/ex26.pp
+++ mathex/ex26.pp
@@ -1,32 +1,31 @@
-Program Example26;
+program Example26;
 
 { Program to demonstrate the MaxValue function. }
 
 { Make sore integer is 32 bit}
 {$mode objfpc}
 
-Uses math;
+uses math;
 
-Type
-  TExFloatArray = Array[1..100] of Float;
-  TExIntArray = Array[1..100] of Integer;
+var i:1..100;
+    f_array:array[1..100] of Float;
+    i_array:array[1..100] of Integer;
+    Pf_array:Pfloat;
+    PI_array:Pinteger;
 
-Var
-  I : Integer;
-  ExFloatArray : TExFloatArray;
-  ExIntArray : TExIntArray;
-  AFLoatArray : PFLoat;
-  AIntArray : PInteger;
 begin
-  Randomize;
-  AFloatArray:=@ExFloatArray[1];
-  AIntArray:=@ExIntArray[1];
-  for I:=1 to 100 do
-    ExFloatArray[i]:=(Random-Random)*100;
-  for I:=1 to 100 do
-    ExIntArray[i]:=Random(I)-Random(100);
-  Writeln('Max Float       : ',MaxValue(ExFloatArray):8:4);
-  Writeln('Max Float   (b) : ',MaxValue(AFloatArray,100):8:4);
-  Writeln('Max Integer     : ',MaxValue(ExIntArray):8);
-  Writeln('Max Integer (b) : ',MaxValue(AIntArray,100):8);
+  randomize;
+
+  Pf_array:=@f_array[1];
+  Pi_array:=@i_array[1];
+
+  for i:=low(f_array) to high(f_array) do
+    f_array[i]:=(random-random)*100;
+  for i:=low(i_array) to high(i_array) do
+    i_array[i]:=random(I)-random(100);
+
+  Writeln('Max Float       : ',MaxValue(f_array):8:4);
+  Writeln('Max Float   (b) : ',MaxValue(Pf_array,100):8:4);
+  Writeln('Max Integer     : ',MaxValue(i_array):8);
+  Writeln('Max Integer (b) : ',MaxValue(Pi_array,100):8);
 end.
