72889177123996ed5672aaa23e7e31ec10101074
daniel <daniel@freepascal.org>
2006-06-10 22:30:04 +0000

    Simplify example.
    git-svn-id: trunk@124 -

--- mathex/ex43.pp
+++ mathex/ex43.pp
@@ -4,16 +4,13 @@ Program Example40;
 
 Uses Math;
 
-Type
-  TExArray = Array[1..10000] of Float;
-
 Var
   I : Integer;
-  ExArray : TExArray;
+  ExArray : Array[1..10000] of Float;
 
 begin
   Randomize;
-  for I:=1 to 10000 do
+  for I:=low(ExArray) to high(ExArray) do
     ExArray[i]:=Randg(1,0.2);
   Writeln('StdDev     : ',StdDev(ExArray):8:4);
   Writeln('StdDev (b) : ',StdDev(@ExArray[0],10000):8:4);
