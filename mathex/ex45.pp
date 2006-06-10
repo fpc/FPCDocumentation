3c93eff26c9552b3ddc54bb991c364a2fcd4fb25
daniel <daniel@freepascal.org>
2006-06-10 22:32:17 +0000

    * Use low and high, simplify
    git-svn-id: trunk@127 -

--- mathex/ex45.pp
+++ mathex/ex45.pp
@@ -4,16 +4,13 @@ Program Example45;
 
 Uses math;
 
-Type
-  TExArray = Array[1..100] of Float;
-
 Var
-  I : Integer;
-  ExArray : TExArray;
+  I : 1..100;
+  ExArray : Array[1..100] of Float;
 
 begin
   Randomize;
-  for I:=1 to 100 do
+  for I:=low(ExArray) to high(ExArray) do
     ExArray[i]:=(Random-Random)*100;
   Writeln('Max             : ',MaxValue(ExArray):8:4);
   Writeln('Min             : ',MinValue(ExArray):8:4);
