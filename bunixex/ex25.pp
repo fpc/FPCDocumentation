09bfe3f490cd2377ede45eac15f039e7a6820e8e
marco <marco@freepascal.org>
2007-05-11 15:01:53 +0000

    * unit dos now used for gettime/date
    git-svn-id: trunk@276 -

--- bunixex/ex25.pp
+++ bunixex/ex25.pp
@@ -2,15 +2,15 @@ Program Example25;
 
 { Program to demonstrate the UTime function. }
 
-Uses BaseUnix,Unix,UnixUtil;
+Uses Dos,BaseUnix,Unix,UnixUtil;
 
 Var utim : utimbuf;
-    year,month,day,hour,minute,second : Word;
+    dow,msec,year,month,day,hour,minute,second : Word;
 
 begin
   { Set access and modification time of executable source }
-  GetTime (hour,minute,second);
-  GetDate (year,month,day);
+  GetTime (hour,minute,second,msec);
+  GetDate (year,month,day,dow);
   utim.actime:=LocalToEpoch(year,month,day,hour,minute,second);
   utim.modtime:=utim.actime;
   if Fputime('ex25.pp',@utim)<>0 then
