40514cb8ce16ad7423096488ffa358a072319087
marco <marco@freepascal.org>
2011-06-06 20:28:46 +0000

    * fixed fillchar<->fillbyte, mantis #19489
    git-svn-id: trunk@790 -

--- refex/ex103.pp
+++ refex/ex103.pp
@@ -9,7 +9,7 @@ begin
   For i:=10 downto 0 do
     begin
     { Fill S with i bytes }
-    FillChar (S,SizeOf(S),32);
+    FillByte (S,SizeOf(S),32);
     { Set Length }
     SetLength(S,I);
     Writeln (s,'*');
