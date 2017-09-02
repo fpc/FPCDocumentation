a88984e10aadeef49b2d37e3bc8dfb464a1c4fb5
michael <michael@freepascal.org>
2017-09-02 10:21:12 +0000

    * Fix bug ID 32328, better explanation of ExtractRelativePath handling of directories
    git-svn-id: trunk@1421 -

--- sysutex/ex35.pp
+++ sysutex/ex35.pp
@@ -12,8 +12,14 @@ begin
 end;
 
 Begin
- Testit ('/pp/src/compiler','/pp/bin/win32/ppc386');
- Testit ('/pp/bin/win32/ppc386','/pp/src/compiler');
- Testit ('e:/pp/bin/win32/ppc386','d:/pp/src/compiler');
- Testit ('e:\pp\bin\win32\ppc386','d:\pp\src\compiler');
+ Testit ('/pp/src/compiler/','/pp/bin/win32/ppc386/');
+ Testit ('/pp/bin/win32/ppc386/','/pp/src/compiler/');
+ Testit ('/pp/bin/win32/','/pp/src/compiler/ppcx386/');
+ Testit ('/pp/bin/win32/','/pp/src/compiler/ppcx386');
+ Testit ('/pp/bin/win32','/pp/src/compiler/ppcx386');
+ Testit ('e:/pp/bin/win32/ppc386/','d:/pp/src/compiler/');
+ Testit ('e:\pp\bin\win32\ppc386/','d:\pp\src\compiler/');
+ Testit ('C:\FPC\3.0.2\','C:\FPC\3.0.2\');
+ Testit ('C:\FPC\3.0.2\','C:\FPC\3.0.4rc1\');
+ Testit ('Q:\','Q:\FPC\3.0.4rc1\');
 End.
