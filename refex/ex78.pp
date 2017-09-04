993fd18ef1b784c5e0c7982e354821c467aa8611
pierre <pierre@freepascal.org>
2017-09-04 15:24:04 +0000

    Disable, as power is now in math unit
    git-svn-id: trunk@1428 -

--- refex/ex78.pp
+++ refex/ex78.pp
@@ -3,5 +3,6 @@ Program Example78;
 { Program to demonstrate the Power function. }
 
 begin
-  Writeln (Power(exp(1.0),1.0):8:2); { Should print 2.72 }
+  // Writeln (Power(exp(1.0),1.0):8:2); { Should print 2.72 }
+  Writeln('Power function is now part of math unit');
 end.
