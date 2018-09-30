16efd83b529fbd02937f9ae220e95c8ce70e45a2
michael <michael@freepascal.org>
2018-09-30 08:36:16 +0000

    * PChar consts can no longer be modified in-place, they are truly read-only
    git-svn-id: trunk@1510 -

--- stringex/ex14.pp
+++ stringex/ex14.pp
@@ -8,8 +8,13 @@ Const
     P1 : PChar = 'THIS IS AN UPPERCASE PCHAR STRING';
     P2 : PChar = 'this is a lowercase string';
 
+
 begin
+  P1:=StrNew(P1);
+  P2:=strNew(P2);
   Writeln ('Uppercase : ',StrUpper(P2));
-  StrLower (P1);
+  StrLower(P1);
   Writeln ('Lowercase : ',P1);
+  StrDispose(P1);
+  StrDispose(P2);
 end.
