f46c0ff8233f2292b21e208ca4c172367427633e
marcoonthegit <marcov@freepascal.org>
2021-12-30 17:56:03 +0100

    * fixes to html compilation script due to directory changes

--- fixdocs.sh
+++ fixdocs.sh
@@ -5,14 +5,17 @@
 # 
 # Needs more errorchecking.
 
-export FPCSRCDIR=/fpc/fpc
+export FPCSRCDIR=/fpc/fpctrunk
 make HTMLFMT=chm html CSSFILE=$FPCSRCDIR/utils/fpdoc/fpdoc.css FPDOC=fpdoc FPCSRCDIR=$FPCSRCDIR 2>&1 |tee buildlog.txt
 
+cd src
 fpc relinkdocs.pp
 fpc compilelatexchm.pp
 fpc gentoc
+cd ..
 
-./relinkdocs
+cd dist/html
+../../src/relinkdocs
 rm -rf prog-old
 rm -rf ref-old
 rm -rf user-old
@@ -32,13 +35,14 @@ cp user-old/*.css user
 cp ref-old/*.png ref
 cp ref-old/*.kwd ref
 cp ref-old/*.css ref
-cp -r pics prog/
-cp -r pics user/
-./compilelatexchm prog "Programmer's Guide"
-./compilelatexchm user "User's Guide"
-./compilelatexchm ref "Reference Guide"  ref/ref.kwd
-./compilelatexchm fpdoc "FPDoc documentation"
+cp -r ../../pics prog/
+cp -r ../../pics user/
+cp -r ../../pics .
+../../src/compilelatexchm prog "Programmer's Guide"
+../../src/compilelatexchm user "User's Guide"
+../../src/compilelatexchm ref "Reference Guide"  ref/ref.kwd
+../../src/compilelatexchm fpdoc "FPDoc documentation"
  
-./gentoc . .
+../../src/gentoc . .
 
 rm fpdoc.kwd user.kwd prog.kwd
