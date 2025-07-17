c85ef3f2b459f96187e68f756eb5227aa0c4c4ef
Michaël Van Canneyt <michael@freepascal.org>
2025-07-17 23:09:26 +0200

    * Fix socket client/server examples. Fixes issue #39427

--- examples/sockex/sockcli.pp
+++ examples/sockex/sockcli.pp
@@ -5,7 +5,8 @@ Program Client;
   Client Version, First Run sock_svr to let it create a socket and then
   sock_cli to connect to that socket
 }
-
+{$mode objfpc}
+{$h+}
 uses Sockets;
 
 procedure PError(const S : string);
@@ -17,29 +18,50 @@ end;
 
 Var
   SAddr    : TInetSockAddr;
-  Buffer   : string [255];
-  S        : Longint;
-  Sin,Sout : Text;
+  Buffer   : string;
+  fd,err        : Longint;
   i        : integer;
+  
+procedure send(const msg : string);
 
 begin
-  S:=fpSocket (AF_INET,SOCK_STREAM,0);
-  if s=-1 then
+  fpSend(fd,@msg[1],length(msg),0);
+end;
+
+function Receive(var S : string) : boolean;
+
+var
+  nRead : integer;
+
+begin
+  SetLength(S,255);
+  nRead:=fpRecv(fd,@S[1],255,0);
+  Result:=nRead>0;
+  if Result then
+    SetLength(S,nRead)
+  else
+    SetLength(S,0);  
+end;
+  
+
+begin
+  fd:=fpSocket (AF_INET,SOCK_STREAM,0);
+  if fd=-1 then
    Perror('Client : Socket : ');
   SAddr.sin_family:=AF_INET;
   { port 50000 in network order }
   SAddr.sin_port:=htons(5000);
   { localhost : 127.0.0.1 in network order }
   SAddr.sin_addr.s_addr:=HostToNet((127 shl 24) or 1);
-  if not Connect (S,SAddr,Sin,Sout) then
-   PError('Client : Connect : ');
-  Reset(Sin);
-  ReWrite(Sout);
-  Buffer:='This is a textstring sent by the Client.';
-  for i:=1 to 10 do
-    Writeln(Sout,Buffer);
-  Flush(Sout);
-  Readln(SIn,Buffer);
-  WriteLn(Buffer);
-  Close(sout);
+  err:=fpConnect(fd,@SAddr,Sizeof(SAddr));
+  if err=-1 then
+   PError('Client : Connect : ')
+  else  
+    begin
+    if Receive(Buffer) then
+       WriteLn(Buffer);
+    Buffer:='This is a textstring sent by the Client.';
+    Send(Buffer);
+    end;   
+  CloseSocket(fd);
 end.
--- examples/sockex/socksvr.pp
+++ examples/sockex/socksvr.pp
@@ -1,32 +1,56 @@
 Program server;
 
+{$mode objfpc}
+{$h+}
+
 {
   Program to test Sockets unit by Michael van Canneyt and Peter Vreman
   Server Version, First Run sock_svr to let it create a socket and then
   sock_cli to connect to that socket
 }
-{$mode fpc}
 uses Sockets;
 
 Var
   FromName : string;
-  Buffer   : string[255];
-  S        : Longint;
-  Sin,Sout : Text;
-  SAddr    : TInetSockAddr;
+  Buffer   : string;
+  S,fd    : Longint;
+  len : longword;
+  SAddr,SRemote : TInetSockAddr;
 
-procedure perror (const S:string);
+procedure perror (const Msg:string);
 begin
-  writeln (S,SocketError);
+  writeln (Msg,': ',SocketError);
   halt(100);
 end;
+
+procedure send(const msg : string);
+
+begin
+  fpSend(fd,@msg[1],length(msg),0);
+end;
+
+function Receive(var S : string) : boolean;
+
+var
+  nRead : integer;
+
+begin
+  SetLength(S,255);
+  nRead:=fpRecv(fd,@S[1],255,0);
+  Result:=nRead>0;
+  if Result then
+    SetLength(S,nRead)
+  else
+    SetLength(S,0);  
+end;
+
 
 begin
   S:=fpSocket (AF_INET,SOCK_STREAM,0);
   if SocketError<>0 then
    Perror ('Server : Socket : ');
   SAddr.sin_family:=AF_INET;
-  { port 50000 in network order }
+  { port 5000 in network order }
   SAddr.sin_port:=htons(5000);
   SAddr.sin_addr.s_addr:=0;
   if fpBind(S,@SAddr,sizeof(saddr))=-1 then
@@ -34,15 +58,18 @@ begin
   if fpListen (S,1)=-1 then
    PError ('Server : Listen : ');
   Writeln('Waiting for Connect from Client, run now sock_cli in an other tty');
-  if Accept(S,FromName,Sin,Sout) then
-   PError ('Server : Accept : '+fromname);
-  Reset(Sin);
-  ReWrite(Sout);
-  Writeln(Sout,'Message From Server');
-  Flush(SOut);
-  while not eof(sin) do
-   begin
-     Readln(Sin,Buffer);
-     Writeln('Server : read : ',buffer);
-   end;
+  len:=sizeof(SRemote);
+  fd:=fpAccept(S, @SRemote,@len);
+  if fd=-1 then
+    PError ('Server : Accepting connection')
+  else
+    begin 
+    Send('Message From Server');
+    while Receive(Buffer) do
+      begin
+      Writeln('Server : read : ',Buffer);
+      end;
+    fpShutdown(fd,2);
+    end;  
+  CloseSocket(s);
 end.
