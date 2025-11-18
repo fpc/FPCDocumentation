282bca5fd42764d31689d387f98aaac2c29a3dde
Michaël Van Canneyt <michael@freepascal.org>
2025-11-18 20:00:48 +0100

    * Some details about ffGeneral in floattostrf. Fixes issue #39444

--- examples/sysutex/ex68.pp
+++ examples/sysutex/ex68.pp
@@ -24,16 +24,41 @@ begin
         end;
 end;
 
+procedure Test2;
+
+var
+  F : Double;
+
+begin
+  F:=0.99411;
+  Writeln('0,0: ',floattostrF(F,FFgeneral,0,0));//=> 0.99,
+  Writeln('1,0: ',floattostrF(F,FFgeneral,1,0));//=> 0.99
+  Writeln('2,0: ',floattostrF(F,FFgeneral,2,0));//=> 0.99
+  Writeln('3,0: ',floattostrF(F,FFgeneral,3,0));//=> 0.994
+  F:=0.99511;
+  Writeln('0,0: ',floattostrF(F,FFgeneral,0,0));//=> 1
+  Writeln('1,0: ',floattostrF(F,FFgeneral,1,0));//=> 1
+  Writeln('2,0: ',floattostrF(F,FFgeneral,2,0));//=> 1
+  Writeln('3,0: ',floattostrF(F,FFgeneral,3,0));//=> 0.995
+  F:=12.145;
+  Writeln('0,0: ',floattostrF(F,FFgeneral,0,0));//=> 12
+  Writeln('1,0: ',floattostrF(F,FFgeneral,1,0));//=> 12
+  Writeln('2,0: ',floattostrF(F,FFgeneral,2,0));//=> 12
+  Writeln('3,0: ',floattostrF(F,FFgeneral,3,0));//=> 12.1
+  Writeln('4,0: ',floattostrF(F,FFgeneral,4,0));//=> 12.15
+  Writeln('5,0: ',floattostrF(F,FFgeneral,5,0));//=> 12.145
+end;
+
 Begin
-  Testit (1.1);
-  Testit (1.1E1);
-  Testit (1.1E-1);
-  Testit (1.1E5);
-  Testit (1.1E-5);
-  Testit (1.1E10);
-  Testit (1.1E-10);
-  Testit (1.1E15);
-  Testit (1.1E-15);
-  Testit (1.1E100);
-  Testit (1.1E-100);
+  Testit (1.1));
+  Testit (1.1E1));
+  Testit (1.1E-1));
+  Testit (1.1E5));
+  Testit (1.1E-5));
+  Testit (1.1E10));
+  Testit (1.1E-10));
+  Testit (1.1E15));
+  Testit (1.1E-15));
+  Testit (1.1E100));
+  Testit (1.1E-100));
 End.
